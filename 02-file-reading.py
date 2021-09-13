bd57b36d0bb897829f919067692dec597915e71e
epover <endliss@sina.cn>
2021-09-13 14:04:57 +0800

    文件读写，序列化

---
+++ 02-file-reading.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+------------------------------
+# @Date     :2021/9/13 13:58
+# @Author   :epover
+# @Email    :endliss@sina.cn
+------------------------------
+"""
+
+# 读取文件，r读模式
+fp = open('text.txt','r')
+data = fp.read()
+print(data)
---
+++ 02-file-write.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+------------------------------
+# @Date     :2021/9/13 13:58
+# @Author   :epover
+# @Email    :endliss@sina.cn
+------------------------------
+"""
+# 写入文件名称，w写入模式
+fp = open('text.txt','w')
+fp.write('hello world')
+fp.close()
