0dd58d747a479ba363ad97f5d3b5a95389061843
epover <endliss@sina.cn>
2021-09-16 20:27:56 +0800

    update code specification

--- code/05-crawler_urllib_ajax_post_kfc.py
+++ code/05-crawler_urllib_ajax_post_kfc.py
@@ -22,16 +22,18 @@ def creat_request(pageIndex):
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
     }
     new_data = urllib.parse.urlencode(data).encode('utf-8')
-    return urllib.request.Request(url=url,data=new_data)
+    return urllib.request.Request(url=url,data=new_data,headers=headers)
 
 def get_content(request):
     response = urllib.request.urlopen(request,context=ssl.SSLContext())
     return response.read().decode('utf-8')
 
+def download(page,content):
+    with open('kfc_' + str(page) + '.json', 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
 if __name__ == '__main__':
     for page in range(1,11):
-        request = creat_request(page)
-        content = get_content(request)
-        with open('kfc'+str(page)+'.json','w') as f:
-            f.write(content)
+        download(page,get_content(creat_request(page)))
     print('over!!!')
