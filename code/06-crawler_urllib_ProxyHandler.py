aae4f9dcc91035f52378d3b1438a02dc81629a9e
epover <endliss@sina.cn>
2021-09-17 22:00:25 +0800

    通过ProxyHandler代理ip爬取数据

--- code/06-crawler_urllib_ProxyHandler.py
+++ code/06-crawler_urllib_ProxyHandler.py
@@ -6,6 +6,10 @@
 # @Email    :endliss@sina.cn
 ------------------------------
 """
+'''
+ 通过ProxyHandler代理ip爬取数据
+'''
+
 import urllib.request
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
