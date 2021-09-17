570919095cb7f7b795ca168667a9256615ffb0d8
epover <endliss@sina.cn>
2021-09-17 22:38:09 +0800

    通过ProxyHandler代理ip爬取数据

--- code/06-crawler_urllib_ProxyHandler.py
+++ code/06-crawler_urllib_ProxyHandler.py
@@ -15,15 +15,16 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def request():
-    url = 'https://www.baidu.com/s?ie=utf-8&f=3&rsv_bp=1&tn=baidu&wd=ip%E5%9C%B0%E5%9D%80%E6%9F%A5%E8%AF%A2'
+    url = 'http://www.baidu.com/s?wd=ip'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+        'Referer': 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=ip%E6%9F%A5%E8%AF%A2&oq=ip%25E6%259F%25A5%25E8%25AF%25A2&rsv_pq=93fa920200008ca9&rsv_t=e38bd2devLnU2pnYyuiCFdtzwh3I3gTa0RM85dJNYYIowWwE%2BMJwdn%2FecFA&rqlang=cn&rsv_dl=tb&rsv_enter=1&rsv_btype=t&inputT=5&rsv_sug3=9&rsv_sug1=3&rsv_sug7=100&rsv_sug2=0&rsv_sug4=721'
     }
     return urllib.request.Request(url=url,headers=headers)
 
 def ProxyHandler():
     proxies = {
-        'http':'58.243.29.58:4554',
+        'http':'27.44.215.155:4513',
     }
     return urllib.request.ProxyHandler(proxies=proxies)
 
