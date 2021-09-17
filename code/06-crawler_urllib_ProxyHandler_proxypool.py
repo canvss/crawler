5b0369c97a425cf15c0a825ab8643259c9c9a30a
epover <endliss@sina.cn>
2021-09-17 22:37:47 +0800

    ProxyHandler创建代理池爬取数据

--- code/06-crawler_urllib_ProxyHandler_proxypool.py
+++ code/06-crawler_urllib_ProxyHandler_proxypool.py
@@ -14,6 +14,7 @@ import ssl,urllib.request,random
 url = 'http://www.baidu.com/s?wd=ip'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Referer':'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=ip%E6%9F%A5%E8%AF%A2&oq=ip%25E6%259F%25A5%25E8%25AF%25A2&rsv_pq=93fa920200008ca9&rsv_t=e38bd2devLnU2pnYyuiCFdtzwh3I3gTa0RM85dJNYYIowWwE%2BMJwdn%2FecFA&rqlang=cn&rsv_dl=tb&rsv_enter=1&rsv_btype=t&inputT=5&rsv_sug3=9&rsv_sug1=3&rsv_sug7=100&rsv_sug2=0&rsv_sug4=721'
 }
 proxies_pool = [
     {'http':'58.243.29.180:4570'},
